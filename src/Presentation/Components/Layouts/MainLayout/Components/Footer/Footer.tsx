ceaf82e75cd3c2ed278a197f20ed2fece693658e
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2025-11-25 23:39:45 -0600

    formateo

--- src/Presentation/Components/Layouts/MainLayout/Components/Footer/Footer.tsx
+++ src/Presentation/Components/Layouts/MainLayout/Components/Footer/Footer.tsx
@@ -3,9 +3,9 @@ import styles from "./Footer.module.scss";
 export const Footer = () => {
   return (
     <footer className={styles.footer} role="contentinfo">
-        <div className="container">
-            <p>&copy; 2025 Saúl Antonio Morquecho Cela. Todos los derechos reservados.</p>
-        </div>
+      <div className="container">
+        <p>&copy; 2025 Saúl Antonio Morquecho Cela. Todos los derechos reservados.</p>
+      </div>
     </footer>
   );
 };
