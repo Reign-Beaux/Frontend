ed7b05c30deb9cc060c44e2101935b37906d8d68
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2025-12-14 20:08:09 -0600

    Se agrega primera parte del blog de los secrets

---
+++ src/Presentation/Pages/Blog/Articles/Secrets/Secrests.tsx
@@ -0,0 +1,175 @@
+import styles from "../Articles.module.scss";
+
+export const Secrets = () => {
+  return (
+    <article className={styles.articleContainer}>
+      <h1 className={styles.articleTitle}>Secretos de Usuario y Variables de Entorno en proyectos .NET</h1>
+
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>¿Qué son los User Secrets?</h2>
+        <p>
+          Los User Secrets (Secretos de usuario) son una funcionalidad diseñada para almacenar datos sensibles
+          durante el desarrollo local sin exponerlos a tu repositorio de código fuente.
+        </p>
+      </section>
+
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>¿Qué hacen y para qué sirven?</h2>
+        <ol>
+          <li>
+            Almacenan información sensible como contraseñas o claves API de forma segura durante el desarrollo
+            local.
+          </li>
+          <li>Evitan que dichos datos se suban accidentalmente a tu repositorio de código fuente (Git).</li>
+          <li>
+            Proporcionan una capa adicional de seguridad al no exponer información sensible en el código
+            fuente.
+          </li>
+        </ol>
+        <p>
+          <strong>Importante</strong>: Los User Secrets no están encriptados y existen como texto plano en tu
+          máquina. Esto significa que otros usuarios que accedan a tu perfil del usuario podrían leerlos.c
+        </p>
+      </section>
+
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>¿Qué son las variables de entorno?</h2>
+        <p>
+          Las variables de entorno son valores que se definen en el sistema operativo y pueden ser leídos por
+          las aplicaciones durante su ejecución. Son útiles para almacenar configuraciones sensibles como
+          contraseñas o claves API, evitando exponerlas directamente en el código fuente.
+        </p>
+      </section>
+
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>¿Qué hacen y para qué sirven?</h2>
+        <ol>
+          <li>
+            Permiten separar la configuración sensible del código fuente, mejorando la seguridad de la
+            aplicación.
+          </li>
+          <li>
+            Facilitan la gestión de configuraciones diferentes para distintos entornos (desarrollo,
+            producción, etc.) sin modificar el código.
+          </li>
+          <li>
+            Son accesibles por cualquier aplicación que se ejecute en el sistema, lo que las hace versátiles
+            para diferentes escenarios.
+          </li>
+        </ol>
+      </section>
+
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>Configuración y uso</h2>
+        <ol>
+          <li>
+            En nuestro Program.cs y lo único que debemos hacer es lo siguiente después de definir en builder:
+            
+            <pre>
+              <code>
+{`
+var builder = WebApplication.CreateBuilder(args);
+// Configuración de Variables de entorno, Secretos de usuarios y appsettings.json
+builder.Configuration
+	.SetBasePath(Directory.GetCurrentDirectory())
+	.AddEnvironmentVariables()
+	.AddUserSecrets<Program>()
+	.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+	.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+`}
+              </code>
+            </pre>
+          </li>
+          <li>
+            Inicializamos los User Secrets en nuestro proyecto con el siguiente comando:
+            <pre>
+              <code>dotnet user-secrets init</code>
+            </pre>
+          </li>
+          <li>
+            Para saber si un proyecto ya fue configurado con User Secrets debemos revisar el archivo .csproj y
+            verificar que contenga una línea similar a esta:
+            <pre>
+              <code>{`<UserSecretsId>your-guid-here</UserSecretsId>`}</code>
+            </pre>
+          </li>
+          <li>
+            Agregamos una clave-valor a los User Secrets con el siguiente comando:
+            <pre>
+              <code>{'dotnet user-secrets set "MiClaveSecreta" "ValorSecreto"'}</code>
+            </pre>
+            En formato JSON esto se vería así:
+            <pre>
+              <code>
+{`
+{
+  "MiClaveSecreta": "ValorSecreto"
+}
+`}
+              </code>
+            </pre>
+          </li>
+          <li>
+            Para agregar una clave-objecto:
+            <pre>
+              <code>
+{`dotnet user-secrets set "ConnectionStrings:FirstDataBase" "FirstPassword"`}
+{`dotnet user-secrets set "ConnectionStrings:SecondDataBase" "SecondPassword"`}
+              </code>
+            </pre>
+            En formato JSON esto se vería así:
+            <pre>
+              <code>
+{`
+{
+  "ConnectionStrings": {
+    "FirstDataBase": "FirstPassword",
+    "SecondDataBase": "SecondPassword"
+  }
+}
+`}
+              </code>
+            </pre>
+          </li>
+          <li>
+            Para agregar una clave-arreglo:
+            <pre>
+              <code>
+{`dotnet user-secrets set "AllowedHosts:0" "localhost"`}
+{`dotnet user-secrets set "AllowedHosts:1" "mydomain.com"`}
+              </code>
+            </pre>
+            En formato JSON esto se vería así:
+            <pre>
+              <code>
+{`
+{
+  "AllowedHosts": [
+    "localhost",
+    "mydomain.com"
+  ]
+}
+`}
+              </code>
+            </pre>
+          </li>
+          <li>
+            Para ver todos los User Secrets almacenados:
+            <pre>
+              <code>dotnet user-secrets list</code>
+            </pre>
+          </li>
+        </ol>
+      </section>
+      
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>Conclusión</h2>
+        <p>
+          Los User Secrets y las Variables de Entorno son herramientas esenciales para manejar
+          configuraciones sensibles en aplicaciones .NET. Al utilizarlas adecuadamente, podemos mejorar la
+          seguridad y flexibilidad de nuestras aplicaciones durante el desarrollo y despliegue.
+        </p>
+      </section>
+    </article>
+  );
+};
