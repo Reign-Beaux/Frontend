ea69e607d20555832060e186f2f1f6c173fc502a
Saúl Antonio Morquecho Cela <samc920130@hotmail.com>
2025-12-27 17:19:57 -0600

    correcciones

--- src/Presentation/Pages/Blog/Articles/Secrets/Secrets.tsx
+++ src/Presentation/Pages/Blog/Articles/Secrets/Secrets.tsx
@@ -60,14 +60,13 @@ export const Secrets = () => {
       </section>
 
       <section className={styles.articleSectionContent}>
-        <h2 className={styles.articleSubtitle}>Configuración y uso</h2>
+        <h2 className={styles.articleSubtitle}>Configuración y uso de User Secrets</h2>
         <ol>
           <li>
             En nuestro Program.cs y lo único que debemos hacer es lo siguiente después de definir en builder:
-            
             <pre>
               <code>
-{`
+                {`
 var builder = WebApplication.CreateBuilder(args);
 // Configuración de Variables de entorno, Secretos de usuarios y appsettings.json
 builder.Configuration
@@ -101,7 +100,7 @@ builder.Configuration
             En formato JSON esto se vería así:
             <pre>
               <code>
-{`
+                {`
 {
   "MiClaveSecreta": "ValorSecreto"
 }
@@ -113,14 +112,14 @@ builder.Configuration
             Para agregar una clave-objecto:
             <pre>
               <code>
-{`dotnet user-secrets set "ConnectionStrings:FirstDataBase" "FirstPassword"`}
-{`dotnet user-secrets set "ConnectionStrings:SecondDataBase" "SecondPassword"`}
+                {`dotnet user-secrets set "ConnectionStrings:FirstDataBase" "FirstPassword"`}
+                {`dotnet user-secrets set "ConnectionStrings:SecondDataBase" "SecondPassword"`}
               </code>
             </pre>
             En formato JSON esto se vería así:
             <pre>
               <code>
-{`
+                {`
 {
   "ConnectionStrings": {
     "FirstDataBase": "FirstPassword",
@@ -135,14 +134,14 @@ builder.Configuration
             Para agregar una clave-arreglo:
             <pre>
               <code>
-{`dotnet user-secrets set "AllowedHosts:0" "localhost"`}
-{`dotnet user-secrets set "AllowedHosts:1" "mydomain.com"`}
+                {`dotnet user-secrets set "AllowedHosts:0" "localhost"`}
+                {`dotnet user-secrets set "AllowedHosts:1" "mydomain.com"`}
               </code>
             </pre>
             En formato JSON esto se vería así:
             <pre>
               <code>
-{`
+                {`
 {
   "AllowedHosts": [
     "localhost",
@@ -159,15 +158,66 @@ builder.Configuration
               <code>dotnet user-secrets list</code>
             </pre>
           </li>
+          <li>
+            Para eliminar un User Secret específico:
+            <pre>
+              <code> {`dotnet user-secrets remove "MiClaveSecreta"`}</code>
+            </pre>
+          </li>
         </ol>
       </section>
-      
+
       <section className={styles.articleSectionContent}>
-        <h2 className={styles.articleSubtitle}>Conclusión</h2>
+        <h2 className={styles.articleSubtitle}>Configuración de las variables de entorno</h2>
+        <ol>
+          <li>
+            En Windows, puedes establecer 2 tipos de variables de entorno, abriendo una terminal de PowerShell
+            o CMD como administrador y ejecutando el siguiente comando:
+            <pre>
+              <code>{'setx MiVariableEntorno "ValorDeMiVariable" /M'}</code>
+            </pre>
+            El modificador <code>/M</code> indica que la variable se establece a nivel del sistema. Si lo
+            omites, la variable se establecerá solo para el usuario actual.
+          </li>
+          <li>
+            La sintaxis para definir las variables de entorno son similares a definirlos en los Secrets, la
+            diferencia es que no usamos comillas dobles, sino, en su lugar usamos dos guines bajos para
+            representar los niveles de jerarquía.
+            <pre>
+              <code>
+                {`
+setx ConnectionStrings__FirstDataBase "FirstPassword" /M
+setx ConnectionStrings__SecondDataBase "SecondPassword" /M
+`}
+              </code>
+            </pre>
+            Tambien podemos definir arreglos de la siguiente manera:
+            <pre>
+              <code>
+                {`
+setx AllowedHosts__0 "localhost" /M
+setx AllowedHosts__1 "mydomain.com" /M
+`}
+              </code>
+            </pre>
+          </li>
+          <li>
+            Para ver las variables de entorno definidas en Windows, puedes usar el siguiente comando en
+            PowerShell:
+            <pre>
+              <code>Get-ChildItem Env:</code>
+            </pre>
+          </li>
+        </ol>
+      </section>
+      <section className={styles.articleSectionContent}>
+        <h2 className={styles.articleSubtitle}>¿Y ahora que hago al desplegar?</h2>
         <p>
-          Los User Secrets y las Variables de Entorno son herramientas esenciales para manejar
-          configuraciones sensibles en aplicaciones .NET. Al utilizarlas adecuadamente, podemos mejorar la
-          seguridad y flexibilidad de nuestras aplicaciones durante el desarrollo y despliegue.
+          La configuración aplicada en el Program.cs permite que tu aplicación lea las variables de entorno
+          configuradas en el servidor así que no debemos hacer otra cosa por esa parte. Sin embargo
+          dependiendo del proveedor que uses para desplegar tu aplicación, la forma de configurar las
+          variables de entorno puede variar. Y eso lo debes revisar en la documentación del proveedor que
+          uses.
         </p>
       </section>
     </article>
